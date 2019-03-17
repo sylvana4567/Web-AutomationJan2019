642780560e09964a21966986ad46acffb648dd23
mrahman <rahmanww@gmail.com>
2019-03-17 19:41:11 -0400

    demo conflict

--- W3Schools/src/main/java/tabledata/TablePage.java
+++ W3Schools/src/main/java/tabledata/TablePage.java
@@ -9,7 +9,7 @@ public class TablePage extends CommonAPI {
     public void readTable(){
        List<String> dataList =  getTextFromWebElements(".w3-table-all.notranslate tr:nth-child(4) td:nth-child(4)");
        for(String st:dataList){
-           System.out.println(st);
+           System.out.println(st + " potential conflict");
        }
     }
 }
